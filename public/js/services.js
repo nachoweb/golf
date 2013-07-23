498d684beb30157bfa8a288a2a02765fce4e6cd4
Nachoweb <nachoelg@gmail.com>
2013-07-23 14:08:16 +0200

    template estadísticas globales

--- public/js/services.js
+++ public/js/services.js
@@ -336,66 +336,7 @@ serv.factory('calculosBoard',function () {
                        more2Percent: 0}
                 }
            };
-           var stats={totales:{},parciales:{}}
-           angular.forEach(tests, function (value, key) {
-               stats.totales={total: (stats.totales["total"] | 0)+value["statistics"]["total"],
-                   goals: 0,
-                   rightBalls: 0,
-                   leftBalls: 0,
-                   longBalls: 0,
-                   shortBalls: 0,
-                   less2: 0,
-                   more2: 0
-               };
-               stats.parciales[value["palo"]]= stats.parciales[value["palo"]] || {};
-               stats.parciales[value["palo"]]={total: (stats.parciales[value["palo"]]["total"] | 0)+value["statistics"]["total"],
-                   goals: 0,
-                   rightBalls: 0,
-                   leftBalls: 0,
-                   longBalls: 0,
-                   shortBalls: 0,
-                   less2: 0,
-                   more2: 0
-               };
-           });
 
-           stats.totales["rightBallsPercent"]=stats.parciales[key]["total"]==0 ? 0 : stats.parciales[key]["rightBalls"]*100/stats.parciales[key]["total"];
-           stats.parciales[key]["leftBallsPercent"]=stats.parciales[key]["total"]==0 ? 0 : stats.parciales[key]["leftBalls"]*100/stats.parciales[key]["total"];
-           stats.parciales.leftBallsPercent
-           stats.parciales.rightBallsPercent
-           stats.parciales.rightBallsPercent
-           stats.parciales.rightBallsPercent
-           stats.parciales.rightBallsPercent
-
-           angular.forEach(stats.parciales, function (value, key) {
-               stats.parciales[key]["rightBallsPercent"]=stats.parciales[key]["total"]==0 ? 0 : stats.parciales[key]["rightBalls"]*100/stats.parciales[key]["total"];
-               stats.parciales[key]["leftBallsPercent"]=stats.parciales[key]["total"]==0 ? 0 : stats.parciales[key]["leftBalls"]*100/stats.parciales[key]["total"];
-               stats.parciales.leftBallsPercent
-               stats.parciales.rightBallsPercent
-               stats.parciales.rightBallsPercent
-               stats.parciales.rightBallsPercent
-               stats.parciales.rightBallsPercent
-           });
-
-           /*// Calculate the total balls
-           var total = 0;
-           for(var i=0; i<test.data.length; i++){
-               for( var j=0; j<test.data.length; j++){
-                   total = total + test.data[i][j];
-               }
-           }
-
-           if(total > 0){
-               // Calculate the statistics
-               localStats.total = total;
-               localStats.goals = test.statistics.goals;
-               localStats.rightBallsPercent = ((test.statistics.rightBalls/total)*100).toFixed(2);
-               localStats.leftBallsPercent = ((test.statistics.leftBalls/total)*100).toFixed(2);
-               localStats.longBallsPercent = ((test.statistics.longBalls/total)*100).toFixed(2);
-               localStats.shortBallsPercent = ((test.statistics.shortBalls/total)*100).toFixed(2);
-               localStats.less2Percent = ((test.statistics.less2/total)*100).toFixed(2);
-               localStats.more2Percent = ((test.statistics.more2/total)*100).toFixed(2);
-           }*/
 
            return localStats;
        }
